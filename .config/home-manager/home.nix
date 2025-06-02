e202a203d6abd77be214ce391a27a43789239afe
0x3mrys <177286108+0x3mrys@users.noreply.github.com>
2025-06-02 16:47:02 +0800

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -11,6 +11,7 @@
     pkgs.htop
     pkgs.lazygit
     pkgs.fzf
+    pkgs.ripgrep
     pkgs.rustup
     pkgs.python314
     pkgs.uv
@@ -29,23 +30,6 @@
     ".config/nvim".source = builtins.toString ../nvim;
     ".tmux.conf".source = ../../.tmux.conf;
   };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/badral/etc/profile.d/hm-session-vars.sh
-  #
 
   services.ssh-agent.enable = true;
   programs.bash = {
